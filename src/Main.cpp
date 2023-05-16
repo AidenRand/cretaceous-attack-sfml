b4d493a968f4924b508e6adfd37cdbea266c2f73
aiden <aidenrphotos@gmail.com>
2023-05-16 10:34:17 -0400

    Add background image

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,7 +6,5 @@ int main()
 	float window_width = 1280;
 	float window_height = 1024;
 	sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Cretaceous Attack");
-	window.setFramerateLimit(60);
-
 	gameFunction(window);
 }
--- src/game.cpp
+++ src/game.cpp
@@ -10,7 +10,15 @@ void gameFunction(sf::RenderWindow& window)
 	float plr_x = 610;
 	float plr_y = 482;
 	Player player(plr_width, plr_height, plr_x, plr_y);
-
+	sf::Texture background_texture;
+	sf::Sprite background;
+	if (!background_texture.loadFromFile("content/background.png"))
+	{
+		std::cout << "ERROR: Cannot load background image";
+	}
+	background.setTexture(background_texture);
+	background.setPosition(sf::Vector2f(0, 0));
+	window.setFramerateLimit(60);
 	while (window.isOpen())
 	{
 
@@ -24,6 +32,7 @@ void gameFunction(sf::RenderWindow& window)
 		}
 		window.clear();
 
+		window.draw(background);
 		player.drawTo(window);
 		player.movePlayer();
 		window.display();
